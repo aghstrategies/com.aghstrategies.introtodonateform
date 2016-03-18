a9ae3c5c31d431dea9e38f1db15ebbaf657508a3
Alice Frumin <alice@aghstrategies.com>
2016-03-18 11:13:54 -0400

    print radio

--- introtodonateform.php
+++ introtodonateform.php
@@ -38,6 +38,9 @@ function introtodonateform_civicrm_buildForm($formName, &$form) {
           }
         }
         //handle amount not found
+        print_r($RadioId);
+        print_r($other);
+        die();
         if (!$found) {
           $defaults['price_' . $RadioId] = 0;
           $defaults['price_' . $other] = $_GET["amount"];
